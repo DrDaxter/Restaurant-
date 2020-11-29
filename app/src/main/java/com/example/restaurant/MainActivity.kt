4b3f0802e0bf7936976b18524a80f0df8b5c5231
DrDaxter <guillermoernestoramirezaguilar@gmail.com>
2020-11-29 07:56:54 -0600

    First commit

---
+++ app/src/main/java/com/example/restaurant/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.restaurant
+
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.preference.PreferenceManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var imgLogo: ImageView
+    lateinit var txtSlogan: TextView
+    lateinit var  txtTagLine: TextView
+    private val key = "STATUS"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        imgLogo = findViewById(R.id.logo)
+        txtSlogan = findViewById(R.id.txt_slogan)
+        txtTagLine = findViewById(R.id.txt_tagline)
+
+        val topAnim: Animation = AnimationUtils.loadAnimation(this,R.anim.top_animation)
+        val bottomAnim: Animation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation)
+
+        imgLogo.startAnimation(topAnim)
+        txtSlogan.startAnimation(bottomAnim)
+        txtTagLine.startAnimation(bottomAnim)
+        NextActivity()
+    }
+
+    fun SavesplashScreemStatus(){
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        var status: Boolean = preferences.getBoolean(key,false)
+        if (status){
+            val intent:Intent = Intent(this,Login::class.java)
+            startActivity(intent)
+        }
+        else{
+            val editor = preferences.edit()
+            editor.putBoolean(key,true)
+            editor.apply()
+            NextActivity()
+        }
+    }
+
+    fun NextActivity(){
+        val login = Runnable {
+            val intent:Intent = Intent(this,Login::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.fade_in,R.anim.fade_out)
+        }
+
+        val handler = Handler()
+        handler.postDelayed(login,5000)
+    }
+
+}
